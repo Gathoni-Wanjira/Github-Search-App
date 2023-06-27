732de5dab81ae540e447acd877960553bfee96ef
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-27 16:10:03 +0300

    Dynamic Nodes

--- script.js
+++ script.js
@@ -24,10 +24,36 @@ form.addEventListener("submit" ,(e)=>{
 
 function displayUsers (details){
     
+    // Getting nodes from the html
+    const avatar = document.querySelector("#avatar")
+    avatar.innerHTML = ''
+    const name = document.querySelector("#name")
+    const about = document.querySelector("#about")
+    const linkRepo = document.querySelector("#link_repo")
+    // Looping data
+   console.log(details)
+    details.forEach((details) => {
+     // Creating elements dynamically
+
+        const p1 = document.createElement("p")
+         p1.textContent = details.name 
+       const p2 = document.createElement("p")
+         p2.textContent = details.about
+         const image = document.createElement("img")
+        image.src = details.avatar_url
+        const repolink = document.createElement("a")
+        repolink.textContent = "Repositories>>"
+         repolink.href = details.repos_url
+
+        //  append child element
+         avatar.appendChild(image)
+         name.appendChild(p1)
+       about.appendChild(p2)
+        linkRepo.appendChild(repolink)
+ console.log(avator.appendChild(image))
+   });
 
 }
-
-
 
 
 
