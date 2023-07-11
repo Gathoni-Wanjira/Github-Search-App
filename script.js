e68a55546d17ce3818818b48470a4695f52bf056
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-12 00:08:19 +0300

    Fetch Data

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
 const accessToken = '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'
-const user = document.querySelector("#EnterUserName")
-const input = document.querySelector("input");
+const formUsername = document.getElementById("user-name-form")
+// const formRepo = document.getElementById("repo-form")
 
-input.addEventListener("submit", (e) => {
-    e.preventDefault()
-    
+formUsername.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const user = e.target.EnterUserName.value
+   
     fetch(`https://api.github.com/users/${user}`, {
-    method :"GET",
-    headers : {
-        Authorization: ` ${accessToken}`,
-        Accept: "application/vnd.github.v3+json"
-    }
+        method: "GET",
+        headers: {
+            Authorization: ` ${accessToken}`,
+            Accept: "application/vnd.github.v3+json"
+        }
+    })
+        .then(response => response.json())
+        .then(userInput => {
+            console.log(userInput);
+            fetchUsers(userInput);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 })
-.then(response => response.json())
-.then(username => {
-    console.log(username);
-})
-.catch(error => {
-    console.error(error);
-  });
-})
+
+function fetchUsers (userInput) {
+
+    const userDetails = document.createElement("div")
+
+
+    const userImg = document.querySelector("#user-img");
+    const userName = document.querySelector("#user-name");
+    const userBio = document.querySelector("#user-bio");
+    const followingLink = document.querySelector("#following");
+    const followersLink = document.querySelector("#followers");
+    const repoBtn = document.querySelector("#repo-btn");
+
+
+    userImg.src = userInput.avatar_url;
+    userName.textContent = userInput.login;
+    userBio.textContent = userInput.bio;
+    followingLink.textContent = userInput.following_url
+    followersLink.textContent = userInput.followers_url  
+    
+
+}
+
+// formRepo.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const user = e.target.EnterRepoName.value
+    
+
+//     fetch(`https://api.github.com/users/${user}/repos`, {
+//         method: "GET",
+//         headers: {
+//             Authorization: ` ${accessToken}`,
+//             Accept: "application/vnd.github.v3+json"
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(userRepository => {
+//             console.log(userRepository);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
+// })
+
+// https://api.github.com/users/Gathoni-Wanjira
