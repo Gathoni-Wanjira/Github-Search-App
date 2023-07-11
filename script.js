bd279b7ca66aa346a03f752c76e37eab23cb7841
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-11 15:39:40 +0300

    Fetch Users

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const accessToken = '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'
+const user = document.querySelector("#EnterUserName")
+const input = document.querySelector("input");
 
+input.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    fetch(`https://api.github.com/users/${user}`, {
+    method :"GET",
+    headers : {
+        Authorization: ` ${accessToken}`,
+        Accept: "application/vnd.github.v3+json"
+    }
+})
+.then(response => response.json())
+.then(username => {
+    console.log(username);
+})
+.catch(error => {
+    console.error(error);
+  });
+})
